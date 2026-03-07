63e6cc3dbc7a3d9d453014519af68684bc7c5b9c
Ihor <kravetsiw@gmail.com>
2026-03-07 22:38:19 +0200

    working MQTT and UART base

--- common/src/logger.cpp
+++
@@ -1,6 +0,0 @@
-#include "logger.hpp"
-#include <iostream>
-
-void Print(){
-    std::cout<<"Hello world\n";
-}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
-#include<iostream>
-#include "logger.hpp"
-int main(){
-    Print();
+#include <iostream>
+#include "UART_Controller.hpp"
+#include "MQTT.hpp"
 
+
+int main(void){
+
+    // Map to struct
+    //UartConfig config = loadConfig("config.json");
+    
+
+    // Task 5: UART Part
+   // runEcho(config);
+
+MQTT_PUBLISH();
+std::cout << "Publish called" << std::endl;
     return 0;
 }
